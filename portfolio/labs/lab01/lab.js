b142fb30e393fd5afb21a2c142da35ff0939479c
r1ddx <52523464+r1ddx@users.noreply.github.com>
2019-07-08 16:23:51 +0800

    Add files via upload

--- portfolio/labs/lab01/lab.js
+++ portfolio/labs/lab01/lab.js
@@ -4,6 +4,31 @@
 
 
 function start(e) {
+        var name = [
+            {
+                char: '王',
+                big5: 'A4FD',
+                cns: '1-455E',
+                unicode: '738B '
+            },
+            {
+                char: '榕',
+                big5: 'BA5F',
+                cns: '1-6928',
+                unicode: '6995'
+            }];
+            var ele = document.getElementById("div1");
+
+            for(var i = 0; i < 3; i++) {
+                ele.innerHTML += name[i].char;
+                ele.innerHTML += ' big5 =';
+                ele.innerHTML += name[i].big5;
+                ele.innerHTML += ' cns =';
+                ele.innerHTML += name[i].cns;
+                ele.innerHTML += ' unicode =';
+                ele.innerHTML += name[i].unicode;
+                ele.innerHTML += '<br>';
+            }
 
 }
 
